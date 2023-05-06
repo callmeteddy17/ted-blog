3f464d4842f7b2b9c060ae2cf59a38c809ed0e28
callmeteddy17 <dtd172000@gmail.com>
2023-05-06 18:48:03 +0700

    resetup

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ app.use(
   cors({
     credentials: true,
     // origin: 'http://localhost:5173',
-    origin: 'https://tedblog.vercel.app',
+    origin: 'http://vercel.app',
   })
 );
 app.use('/upload', express.static(__dirname + '/upload'));
